7d9c64b97e77d48de02bd4f40cdad14e7373fd69
thanh <thanh@MacBook-Pro.local>
2019-02-19 14:18:46 +0700

    Init project

---
+++ MusicCloud/MusicCloud/Scenes/ViewControllers/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  MusicCloud
+//
+//  Created by thanh on 2/19/19.
+//  Copyright © 2019 thanh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
